fd5d32a6e784a96010a5371c9fc441e281069b79
Jaime Pajuelo <jpajuelo@conwet.com>
2015-04-18 16:30:58 +0200

    Add the neccesary helpers to control the inheritance

---
+++ src/core.js
@@ -0,0 +1,85 @@
+/**
+ *  Copyright 2015 Jaime Pajuelo
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+"use strict";
+
+
+/**
+ * @param {Function} parentClass
+ * @returns {Function} The instance on which this method was called.
+ */
+Function.prototype.inherit = function inherit(parentClass) {
+    this.prototype = Object.create(parentClass.prototype);
+    this.prototype.constructor = this;
+
+    // TODO: think a better way to get the parent class.
+    this.prototype.parentClass = parentClass;
+
+    return this;
+};
+
+
+/**
+ * @param {String} memberName
+ * @param {Function} memberMethod
+ * @returns {Function} The instance on which this method was called.
+ */
+Function.prototype.member = function member(memberName, memberMethod) {
+    this.prototype[memberName] = memberMethod;
+
+    return this;
+};
+
+
+/**
+ * @namespace
+ */
+var utils = {
+
+    /**
+     * @param {Object} sourceObject
+     * @returns {Object} The new object cloned.
+     */
+    cloneObject: function cloneObject(sourceObject) {
+        if (sourceObject == null || typeof sourceObject !== 'object') {
+            sourceObject = {};
+        }
+
+        return JSON.parse(JSON.stringify(sourceObject))
+    },
+
+    /**
+     * @param {Object} sourceObject
+     * @param {Object} targetObject
+     * @return {Object} The sourceObject merged with targetObject.
+     */
+    updateObject: function updateObject(sourceObject, targetObject) {
+        var name;
+
+        sourceObject = this.cloneObject(sourceObject);
+        targetObject = this.cloneObject(targetObject);
+
+        for (name in sourceObject) {
+            if (name in targetObject) {
+                sourceObject[name] = targetObject[name];
+            }
+        }
+
+        return sourceObject;
+    }
+
+};
